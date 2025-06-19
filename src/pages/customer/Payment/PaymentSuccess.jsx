cad39a24f70fef006f712b30aba60b19e68b98e2
phamthanh1002 <phamthanh022001@gmail.com>
2025-06-20 03:17:11 +0700

    Update .gitignore to ignore .env and update Firebase + Payment pages

--- src/pages/customer/Payment/PaymentSuccess.jsx
+++ src/pages/customer/Payment/PaymentSuccess.jsx
@@ -6,23 +6,33 @@ const PaymentSuccess = () => {
   const navigate = useNavigate();
 
   return (
-    <Result
-      status="success"
-      title="Thanh toán thành công!"
-      subTitle="Cảm ơn bạn đã mua hàng. Chúng tôi sẽ xử lý đơn hàng của bạn sớm nhất."
-      extra={[
-        <Button
-          style={{ backgroundColor: "black", color: "white" }}
-          key="home"
-          onClick={() => navigate("/")}
-        >
-          Về trang chủ
-        </Button>,
-        <Button key="order" onClick={() => navigate("/orders")}>
-          Xem đơn hàng
-        </Button>,
-      ]}
-    />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Result
+        status="success"
+        title="Thanh toán thành công!"
+        subTitle="Cảm ơn bạn đã mua hàng. Chúng tôi sẽ xử lý đơn hàng của bạn sớm nhất."
+        extra={[
+          <Button
+            style={{ backgroundColor: "black", color: "white" }}
+            key="home"
+            onClick={() => navigate("/")}
+          >
+            Về trang chủ
+          </Button>,
+          <Button key="order" onClick={() => navigate("/orders")}>
+            Xem đơn hàng
+          </Button>,
+        ]}
+      />
+    </div>
   );
 };
 
